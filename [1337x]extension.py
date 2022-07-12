2650ece4d774c831fd8d4a70f0917e3136427e46
Shoaib Ali <coolbrampton@gmail.com>
2022-07-11 20:22:34 -0400

    Small tweaks to project; merged parser functions to static class methods
    - In addition, removed reference to requests_cache as building with PyInstaller was bugged.

---
+++ [1337x]extension.py
@@ -0,0 +1,133 @@
+# --------------------------------
+# File      : [1337x]extension.py
+# Author    : Doomlad
+# Date      : 07/11/2022
+# Info      : Parse a search query via torrent site 1337x.to and its different proxies and
+#             prints an array with relevant torrent information to be processed by EZTorrentio.
+# --------------------------------
+#
+# Copyright 2022 Doomlad
+#
+# Redistribution and use in source and binary forms, with or without modification,
+# are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice,
+# this list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+# this list of conditions and the following disclaimer in the documentation and/or
+# other materials provided with the distribution.
+#
+# 3. Neither the name of the copyright holder nor the names of its contributors may be
+# used to endorse or promote products derived from this software without specific prior
+# written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY
+# EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+# OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT
+# SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+# SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT
+# OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+# HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR
+# TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+# EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+# Build standalone executable with external libs using PyInstaller
+# pyinstaller --noconfirm --onefile --console --icon "<path-to-codebase>.ico" --clean --paths "C:/path-to-codebase/venv/Lib/site-packages" --add-data "C:/<path-to-codebase>/i1337x.py;."  "C:/<path-to-codebase>/[1337x]extension.py"
+
+import textwrap
+import argparse
+from pprint import pprint
+from i1337x import py1337x
+
+
+__version__ = '1.0.0'
+argparse_parser = argparse.ArgumentParser(
+    formatter_class=lambda prog: argparse.RawTextHelpFormatter(prog,max_help_position=40),
+    description=textwrap.dedent('''\
+        EzTorrentioExtension: [1337x.to]
+        --------------------------------
+        Parse a search query via torrent site 1337x.to and its different proxies and
+        prints an array with relevant torrent information to be processed by EZTorrentio.'''))
+
+argparse_parser.add_argument('query', nargs='?', default=None, help='Enter a search query to be parsed via 1337x or Rarbg torrents.')
+
+argparse_parser.add_argument('-P', '--proxy', default='1337x.to', metavar='<url>', 
+    choices=['1337x.to', '1337x.tw', '1377x.to', '1337xx.to', '1337x.st', 'x1337x.ws', 'x1337x.eu', 'x1337x.se', '1337x.is', '1337x.gd'],
+    help=textwrap.dedent('''\
+        Specify an optional proxy site for 1337x.
+            Options: <(default) 1337x.to, 1337x.tw, 1377x.to, 1337xx.to, 
+                               1337x.st, x1337x.ws, x1337x.eu, x1337x.se, 
+                               1337x.is, 1337x.gd>\n\n'''))
+
+argparse_parser.add_argument('-m', '--method', default='search', metavar='<method>', 
+    choices=['search', 'trending', 'top', 'popular', 'browse', 'info'],
+    help=textwrap.dedent('''\
+        Specify the type of action you would like to perform.
+            Options: <(default) search, trending, top, popular, browse, info>\n\n'''))
+    
+argparse_parser.add_argument('-c', '--category', default=None, metavar='<category>', 
+    choices=['Anime', 'Apps', 'Documentaries', 'Games', 'Movies', 'Music', 'Other', 'TV', 'XXX'],
+    help=textwrap.dedent('''\
+        Specify the type of action you would like to perform, this MUST be set when method=popular/browse.
+            Options: <(default) None, Anime, Apps, Documentaries, Games, Movies, Music, Other, TV, XXX>\n\n'''))
+
+argparse_parser.add_argument('-s', '--sortby', default=None, metavar='<criteria>',
+    choices=['time', 'size', 'seeders', 'leechers'],
+    help=textwrap.dedent('''\
+        Specify a sorting criteria when using the search method.
+            Options: <(default) None, time, size, seeders, leechers>\n\n'''))
+
+argparse_parser.add_argument('-o', '--order', default='desc', metavar='<order>',
+    choices=['desc', 'asc'],
+    help=textwrap.dedent('''\
+        Specify the order results are retreived. (Descending/Ascending)
+            Options: <(default) desc, asc>\n\n'''))
+
+argparse_parser.add_argument('-p', '--page', default=1, metavar='<page>',
+    help=textwrap.dedent('''\
+        Specify the page number to pull results from. 
+            Options: <(default=1) integer>\n\n'''))
+
+argparse_parser.add_argument('-w', '--week', default=False, metavar='<bool>',
+    help=textwrap.dedent('''\
+        Specify if you want to search for top results of the week. Only applies to trending and popular search methods.
+            Options: <(default) false, true>\n\n'''))
+
+argparse_parser.add_argument('-C', '--cookie', default=None, metavar='<cookie>',
+    help=textwrap.dedent('''\
+        Specify a CloudFlare protected cookie bypass for certain proxies that use it.
+        Solve the captcha in your browser and copy the value of the 'cf_clearance' cookie.
+            Options: <(default=None) cookie>\n\n'''))
+
+args = argparse_parser.parse_args()
+client = py1337x(args.proxy, args.cookie)
+
+# items:
+#     name, torrentId, link, seeders, leechers, size, time, uploader, uploaderLink
+# currentPage:
+# itemCount:
+# pageCount:
+
+if args.query:
+    result = client.search(args.query, args.page, args.category, args.sortby, args.order)
+
+elif args.method == 'trending':
+    result = client.trending(args.category, args.week)
+
+elif args.method == 'top':
+    result = client.top(args.category)
+
+elif args.method == 'popular':
+    result = client.popular(args.category, args.week)
+
+elif args.method == 'browse':
+    result = client.browse(args.category, args.page)
+
+elif args.method == 'info':
+    result = client.info(args.query)
+
+try:
+    pprint(result)
+except NameError:
+    print()
---
+++ i1337x.py
@@ -0,0 +1,234 @@
+# --------------------------------
+# File      : i1337x.py (renamed from py1337x.py)
+# Author    : Hemanta Pokharel, Doomlad
+# Date      : 07/11/2022
+# Info      : Parse a search query via torrent site 1337x.to and its different proxies and
+#             prints an array with relevant torrent information to be processed by EZTorrentio.
+# --------------------------------
+#
+# MIT License
+
+# Copyright (c) 2021 Hemanta Pokharel
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+import requests
+from bs4 import BeautifulSoup
+
+
+class py1337x:
+    # Remove requests_cache to fix bug in Pyinstaller executable build
+    def __init__(self, proxy=None, cookie=None):
+        self.baseUrl = f'https://www.{proxy}' if proxy else 'https://www.1337x.to'
+        self.headers = {
+            'user-agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:88.0) Gecko/20100101 Firefox/88.0',
+            'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+            'accept-language': 'en-US,en;q=0.5',
+            'upgrade-insecure-requests': '1',
+            'te': 'trailers'
+        }
+
+        if cookie:
+            self.headers['cookie'] = f'cf_clearance={cookie}'
+
+        self.requests = requests
+
+    #: Searching torrents
+    def search(self, query, page=1, category=None, sortBy=None, order='desc'):
+        query = '+'.join(query.split())
+        category = category.upper() if category and category.lower() in ['xxx', 'tv'] else category.capitalize() if category else None
+        url = f"{self.baseUrl}/{'sort-' if sortBy else ''}{'category-' if category else ''}search/{query}/{category+'/' if category else ''}{sortBy.lower()+'/' if sortBy else ''}{order.lower()+'/' if sortBy else ''}{page}/"
+
+        response = self.requests.get(url, headers=self.headers)
+        return self.torrentParser(response, baseUrl=self.baseUrl, page=page)
+
+    #: Trending torrents
+    def trending(self, category=None, week=False):
+        url = f"{self.baseUrl}/trending{'-week' if week and not category else ''}{'/w/'+category.lower()+'/' if week and category else '/d/'+category.lower()+'/' if not week and category else ''}"
+
+        response = self.requests.get(url, headers=self.headers)
+        return self.torrentParser(response, baseUrl=self.baseUrl)
+
+    #: Top 100 torrents
+    def top(self, category=None):
+        category = 'applications' if category and category.lower() == 'apps' else 'television' if category and category.lower() == 'tv' else category.lower() if category else None
+        url = f"{self.baseUrl}/top-100{'-'+category if category else ''}"
+
+        response = self.requests.get(url, headers=self.headers)
+        return self.torrentParser(response, baseUrl=self.baseUrl)
+
+    #: Popular torrents
+    def popular(self, category, week=False):
+        url = f"{self.baseUrl}/popular-{category.lower()}{'-week' if week else ''}"
+
+        response = self.requests.get(url, headers=self.headers)
+        return self.torrentParser(response, baseUrl=self.baseUrl)
+
+    #: Browse torrents by category type
+    def browse(self, category, page=1):
+        category = category.upper() if category.lower() in ['xxx', 'tv'] else category.capitalize()
+        url = f'{self.baseUrl}/cat/{category}/{page}/'
+
+        response = self.requests.get(url, headers=self.headers)
+        return self.torrentParser(response, baseUrl=self.baseUrl, page=page)
+
+    #: Info of torrent
+    def info(self, link=None, torrentId=None):
+        if not link and not torrentId:
+            raise TypeError('Missing 1 required positional argument: link or torrentId')
+        elif link and torrentId:
+            raise TypeError('Got an unexpected argument: Pass either link or torrentId')
+
+        link = f'{self.baseUrl}/torrent/{torrentId}/h9/' if torrentId else link
+        response = self.requests.get(link, headers=self.headers)
+
+        return self.infoParser(response, baseUrl=self.baseUrl)
+
+
+    # Moved parser(s) to static methods in 1337x class
+    @staticmethod
+    def torrentParser(response, baseUrl, page=1):
+        soup = BeautifulSoup(response.content, 'html.parser')
+
+        torrentList = soup.select('a[href*="/torrent/"]')
+        seedersList = soup.select('td.coll-2')
+        leechersList = soup.select('td.coll-3')
+        sizeList = soup.select('td.coll-4')
+        timeList = soup.select('td.coll-date')
+        uploaderList = soup.select('td.coll-5')
+
+        lastPage = soup.find('div', {'class': 'pagination'})
+
+        if not lastPage:
+            pageCount = page
+        else:
+            try:
+                pageCount = int(lastPage.findAll('a')[-1]['href'].split('/')[-2])
+
+            except Exception:
+                pageCount = page
+
+        results = {
+            'items': [],
+            'currentPage': page or 1,
+            'itemCount': len(torrentList),
+            'pageCount': pageCount
+        }
+
+        if torrentList:
+            for count, torrent in enumerate(torrentList):
+                name = torrent.getText().strip()
+                torrentId = torrent['href'].split('/')[2]
+                link = baseUrl+torrent['href']
+                seeders = seedersList[count].getText()
+                leechers = leechersList[count].getText()
+                size = sizeList[count].contents[0]
+                time = timeList[count].getText()
+                uploader = uploaderList[count].getText().strip()
+                uploaderLink = baseUrl+'/'+uploader+'/'
+
+                results['items'].append({
+                    'name': name,
+                    'torrentId': torrentId,
+                    'link': link,
+                    'seeders': seeders,
+                    'leechers': leechers,
+                    'size': size,
+                    'time': time,
+                    'uploader': uploader,
+                    'uploaderLink': uploaderLink
+                })
+
+        return results
+
+    @staticmethod
+    def infoParser(response, baseUrl):
+        soup = BeautifulSoup(response.content, 'html.parser')
+
+        name = soup.find('div', {'class': 'box-info-heading clearfix'})
+        name = name.text.strip() if name else None
+
+        shortName = soup.find('div', {'class': 'torrent-detail-info'})
+        shortName = shortName.find('h3').getText().strip() if shortName else None
+
+        description = soup.find('div', {'class': 'torrent-detail-info'})
+        description = description.find(
+            'p').getText().strip() if description else None
+
+        genre = soup.find('div', {'class': 'torrent-category clearfix'})
+        genre = [i.text.strip() for i in genre.find_all('span')] if genre else None
+
+        thumbnail = soup.find('div', {'class': 'torrent-image'})
+        thumbnail = thumbnail.find('img')['src'] if thumbnail else None
+
+        if thumbnail and not thumbnail.startswith('http'):
+            thumbnail = f'{baseUrl}'+thumbnail
+
+        magnetLink = soup.select('a[href^="magnet"]')
+        magnetLink = magnetLink[0]['href'] if magnetLink else None
+
+        infoHash = soup.find('div', {'class': 'infohash-box'})
+        infoHash = infoHash.find('span').getText() if infoHash else None
+
+        images = soup.find('div', {'class': 'tab-pane active'})
+        images = [i['src'] for i in images.find_all('img')] if images else None
+
+        descriptionList = soup.find_all('ul', {'class': 'list'})
+
+        if len(descriptionList) > 2:
+            firstList = descriptionList[1].find_all('li')
+            secondList = descriptionList[2].find_all('li')
+
+            category = firstList[0].find('span').getText()
+            species = firstList[1].find('span').getText()
+            language = firstList[2].find('span').getText()
+            size = firstList[3].find('span').getText()
+            uploader = firstList[4].find('span').getText().strip()
+            uploaderLink = baseUrl+'/'+uploader+'/'
+
+            downloads = secondList[0].find('span').getText()
+            lastChecked = secondList[1].find('span').getText()
+            uploadDate = secondList[2].find('span').getText()
+            seeders = secondList[3].find('span').getText()
+            leechers = secondList[4].find('span').getText()
+
+        else:
+            category = species = language = size = uploader = uploaderLink = downloads = lastChecked = uploadDate = seeders = leechers = None
+
+        return {
+            'name': name,
+            'shortName': shortName,
+            'description': description,
+            'category': category,
+            'type': species,
+            'genre': genre,
+            'language': language,
+            'size': size,
+            'thumbnail': thumbnail,
+            'images': images if images else None,
+            'uploader': uploader,
+            'uploaderLink': uploaderLink,
+            'downloads': downloads,
+            'lastChecked': lastChecked,
+            'uploadDate': uploadDate,
+            'seeders': seeders,
+            'leechers': leechers,
+            'magnetLink': magnetLink,
+            'infoHash': infoHash.strip() if infoHash else None
+        }
